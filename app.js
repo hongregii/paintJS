e14e14525f6e347b6675371fb9dd4023498a4a37
hongregii <98505239+hongregii@users.noreply.github.com>
2022-04-21 19:13:35 +0900

    2.4
    Brush size, color

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const canvas = document.getElementById("jsCanvas");
 const ctx = canvas.getContext("2d");
+const colors = document.getElementsByClassName("jsColor");
+const range = document.getElementById("jsRange");
 
 canvas.width = canvas.offsetWidth;
 canvas.height = canvas.offsetHeight;
@@ -7,6 +9,9 @@ canvas.height = canvas.offsetHeight;
 ctx.strokeStyle = "#2c2c2c";
 ctx.lineWidth = 2.5;
 
+function changeColor(event) {
+    ctx.strokeStyle = event.target.style.backgroundColor
+}
 
 let painting = false;
 
@@ -34,9 +39,19 @@ function startPainting(event) {
     painting = true;
 }
 
+function handleRangeChange(event) {
+    ctx.lineWidth = event.target.value;
+}
+
 if (canvas) {
     canvas.addEventListener("mousemove", onMouseMove);
     canvas.addEventListener("mousedown", startPainting);
     canvas.addEventListener("mouseup", stopPainting);
     canvas.addEventListener("mouseleave", stopPainting);
+}
+
+Array.from(colors).forEach(color => color.addEventListener("click", changeColor));
+
+if (range) {
+    range.addEventListener('input', handleRangeChange)
 }
